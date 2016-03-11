0e4bd871de8cd8f12dcc2cf31012fd54d8c20351
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2016-03-10 22:01:49 -0300

    * Adicionado exercício SumDouble

--- codingBat/src/warmup1/SumDouble.java
+++ codingBat/src/warmup1/SumDouble.java
@@ -1,5 +1,11 @@
 package warmup1;
 
 public class SumDouble {
+/**
+ * Given two int values, return their sum. Unless the two values are the same, then return double their sum. 
 
+sumDouble(1, 2) = 3
+sumDouble(3, 2) = 5
+sumDouble(2, 2) = 8
+ */
 }
