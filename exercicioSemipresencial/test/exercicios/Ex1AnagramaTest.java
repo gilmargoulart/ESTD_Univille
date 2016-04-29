3f5298afdfbaeb43e7e00d183470835251b2d875
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2016-04-28 21:08:14 -0300

    Adicionado exercicios semipresenciais

---
+++ exercicioSemipresencial/test/exercicios/Ex1AnagramaTest.java
@@ -0,0 +1,31 @@
+package exercicios;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Ex1AnagramaTest {
+	
+	@Test
+	public void testeAnagrama(){
+		
+		//Definir palavra 1
+		String str1 = "Gato";
+		//Definir palavra 2
+		String str2 = "Toga";
+		
+		//
+		Ex1Anagrama anagrama = new Ex1Anagrama(str1, str2);
+		
+		//Verificar se as duas Strings sào anagrama
+		Assert.assertTrue(anagrama.isAnagrama()); //True
+		
+		//Ajustar outro teste, a fim de validar um teste "falso"
+		str1 = "Gato";
+		str2 = "Uhulll";
+		
+		anagrama = new Ex1Anagrama(str1, str2);
+		
+		//Verificar seas duas strings sao anagrama
+		Assert.assertFalse(anagrama.isAnagrama()); //False
+	}
+}
